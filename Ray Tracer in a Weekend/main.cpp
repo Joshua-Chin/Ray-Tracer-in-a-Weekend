306b4f0e94b42c339bfe1ceb9bbaeb0b17ed005d
Joshua Chin <joshuarchin@gmail.com>
2018-05-26 16:34:34 -0400

    Initial Commit

---
+++ Ray Tracer in a Weekend/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ray Tracer in a Weekend
+//
+//  Created by Joshua Chin on 5/26/18.
+//  Copyright © 2018 Joshua Chin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
