1cc659382d985901c9da9d04a4c282873bf20be0
Joshua Chin <joshuarchin@gmail.com>
2018-05-27 09:46:56 -0400

    added lambert diffuse materials and gamma correction.

--- Ray Tracer in a Weekend/main.cpp
+++ Ray Tracer in a Weekend/main.cpp
@@ -17,21 +17,34 @@
 #include "camera.h"
 #include "rng.h"
 
-vec3 color(const ray& r, hittable& h) {
+vec3 random_in_unit_sphere() {
+    std::uniform_real_distribution<float> dist(-1, 1);
+    vec3 p;
+    do {
+        p = vec3(dist(engine), dist(engine), dist(engine));
+    } while (p * p > 1);
+    return p;
+}
+
+vec3 color(const ray& r, hittable& h, float mul=1) {
     hit_record record;
-    if (h.hit_by(r, 0, MAXFLOAT, record)) {
-        vec3 N = record.normal;
-        return 0.5 * vec3(N[0]+1, N[1]+1, N[2]+1);
+    if (mul < 0.001) {
+        return vec3();
+    }
+    if (h.hit_by(r, 0.001, MAXFLOAT, record)) {
+        vec3 target = record.position + record.normal + random_in_unit_sphere();
+        return color(ray(record.position, target - record.position), h, 0.5 * mul);
     }
     vec3 d = r.direction();
     float t = 0.5 * (d.y() + 1);
-    return (1 - t) * vec3(1, 1, 1) + t * vec3(0.5, 0.7, 1);
+    return mul * ((1 - t) * vec3(1, 1, 1) + t * vec3(0.5, 0.7, 1));
 }
 
 int main(int argc, const char * argv[]) {
     int nx = 600;
     int ny = 300;
     int ns = 100;
+    float gamma = 1 / 2.2;
     
     std::vector<std::uint8_t> image;
     image.resize(4 * ny * nx);
@@ -54,9 +67,9 @@ int main(int argc, const char * argv[]) {
                 c += color(r, hl);
             }
             c /= ns;
-            image[4 * nx * i + 4 * j + 0] = 255 * c.r();
-            image[4 * nx * i + 4 * j + 1] = 255 * c.g();
-            image[4 * nx * i + 4 * j + 2] = 255 * c.b();
+            image[4 * nx * i + 4 * j + 0] = 255 * pow(c.r(), gamma);
+            image[4 * nx * i + 4 * j + 1] = 255 * pow(c.g(), gamma);
+            image[4 * nx * i + 4 * j + 2] = 255 * pow(c.b(), gamma);
             image[4 * nx * i + 4 * j + 3] = 255; // A
         }
     }
